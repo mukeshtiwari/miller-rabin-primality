c10961bdc15eb9d45cd397fbd88bf941fd1a9da5
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2012-12-08 03:36:26 +0530

    Added GADT.

--- GData.hs
+++ GData.hs
@@ -1,5 +1,6 @@
 module GData where 
 
+{--
 data Expr = I Int
         | B Bool           -- boolean constants
         | Add Expr Expr
@@ -26,6 +27,22 @@ eval ( Eq e1 e2 ) = case ( eval e1 , eval e2 ) of
                        ( Just ( Left a )  , Just ( Left b ) )  -> Just ( Right ( a == b ) )
                        ( Just ( Right a ) , Just ( Right b ) ) -> Just ( Right ( a == b ) ) -- this will for single expression like B True `Eq` B True
                        ( _ , _ ) -> Nothing  -- No comparision between Int and Bool 
+
+--}
+
+data Expr :: * -> * where 
+  I :: Int -> Expr Int 
+  B :: Bool -> Expr Bool
+  Add :: Expr Int -> Expr Int -> Expr Int 
+  Mul :: Expr Int -> Expr Int -> Expr Int
+  Eq :: Eq a => Expr a -> Expr a  -> Expr Bool
+
+eval :: Expr a -> a 
+eval ( I n ) = n
+eval ( B b ) = b
+eval ( Add e1 e2 ) = eval e1 + eval e2 
+eval ( Mul e1 e2 ) = eval e1 * eval e2 
+eval ( Eq e1 e2 ) = eval e1 == eval e2
                        
 --main :: IO ()
 --main = print.eval $ ( ( I 5 `Add` I 1 ) `Mul` I 7 :: Expr )  `Eq` ( ( I 5 `Add` I 1 ) `Mul` I 7 :: Expr )       
